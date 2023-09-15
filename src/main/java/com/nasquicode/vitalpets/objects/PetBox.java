4317f1682d6a235108c5284aca6dc9b7d3565647
Bletzzi <107274811+BletzziDev@users.noreply.github.com>
2023-09-15 19:05:03 -0300

    Commit

--- src/main/java/com/nasquicode/vitalpets/objects/PetBox.java
+++ src/main/java/com/nasquicode/vitalpets/objects/PetBox.java
@@ -1,4 +1,16 @@
 package com.nasquicode.vitalpets.objects;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+
+@AllArgsConstructor @Getter
 public class PetBox {
+    private String key;
+    private ItemStack stack;
+    private String name;
+    private List<String> lore;
+    private List<String> rewards; // Change this to pet type object instead string
 }
